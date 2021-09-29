114141a524c1d416ab28822811f755ca6ff707b5
Issac Liu <issac.liu.work@gmail.com>
2021-09-29 23:47:05 +0800

    90% completed footer and header

--- src/app/shared/custom-material/custom-material.module.ts
+++ src/app/shared/custom-material/custom-material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 /**
  * decorator NgModule
@@ -11,12 +12,14 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     MatInputModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDividerModule
   ],
   exports: [
     MatInputModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDividerModule
   ]
 })
 export class CustomMaterialModule {}
