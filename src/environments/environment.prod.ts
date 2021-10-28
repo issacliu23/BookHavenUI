31bf44017c5e4c986814759b9199a7cae6b8aa74
yijie-chan <62925588+yijie-chan@users.noreply.github.com>
2021-10-28 23:24:00 +0800

    test http:// in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   API: {
-    BOOK_URL: 'internal-bookhaven-app-load-balancer-1836006333.ap-southeast-1.elb.amazonaws.com',
-    PAYMENT_URL: 'internal-bookhaven-app-load-balancer-1836006333.ap-southeast-1.elb.amazonaws.com',
-    REVIEW_URL: 'internal-bookhaven-app-load-balancer-1836006333.ap-southeast-1.elb.amazonaws.com',
-    USER_URL: 'internal-bookhaven-app-load-balancer-1836006333.ap-southeast-1.elb.amazonaws.com'
+    BOOK_URL: 'http://internal-bookhaven-app-load-balancer-1836006333.ap-southeast-1.elb.amazonaws.com',
+    PAYMENT_URL: 'http://internal-bookhaven-app-load-balancer-1836006333.ap-southeast-1.elb.amazonaws.com',
+    REVIEW_URL: 'http://internal-bookhaven-app-load-balancer-1836006333.ap-southeast-1.elb.amazonaws.com',
+    USER_URL: 'http://internal-bookhaven-app-load-balancer-1836006333.ap-southeast-1.elb.amazonaws.com'
   }
 };
